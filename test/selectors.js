3b2e4d76ab13928aedc862298693463733764219
Khristina2015 <matvienkokhristina1@gmail.com>
2017-09-04 13:45:57 +0300

    'new'

--- test/selectors.js
+++ test/selectors.js
@@ -1,14 +1,14 @@
 describe('Проверка на селекторы ID', function() {
     beforeAll(function(done) {
-        browser.url("https://www.google.com.ua").waitForVisible('#_eEe');
+        browser.url("https://www.google.com.ua").waitForVisible('.logo-subtext');
     });
 
 
-    it('', function(done) {
+    it('Проверка Дженкинса', function(done) {
 
-        browser.url("https://mail.google.com/mail/?tab=wm")
-            // .waitForExist("span=Gmail", 2000);
-			browser.waitForText("Gmail", 2000);
+        browser.url("https://www.google.com.ua/imghp?hl=uk&tab=wi")
+             .waitForExist("#hplogo", 2000);
+			// .waitForText("Gmail", 2000);
         browser.call(done);
     });
 
--- wdio.conf.js
+++ wdio.conf.js
@@ -107,6 +107,9 @@
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: ['selenium-standalone'],
+	 seleniumInstallArgs: {
+        proxy: process.env.PB_PROXY
+    },
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
